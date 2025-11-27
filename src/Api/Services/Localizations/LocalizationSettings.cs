73e1202f6c642e7e1825ae4ef87db67ce162a3ea
Minh Sáng <minhsang.1mil@gmail.com>
2025-11-27 13:28:56 +0700

    chore: adjust set default value logic for support culture

--- src/Api/Services/Localizations/LocalizationSettings.cs
+++ src/Api/Services/Localizations/LocalizationSettings.cs
@@ -3,6 +3,29 @@ namespace Api.Services.Localizations;
 public class LocalizationSettings
 {
     public string DefaultCulture { get; set; } = "vi";
-    public string[] SupportedCultures { get; set; } = [];
+
+    private string[]? _supportedCultures;
+    public string[] SupportedCultures
+    {
+        get { return _supportedCultures!; }
+        set
+        {
+            if (value == null)
+            {
+                _supportedCultures = ["en", "vi"];
+                return;
+            }
+
+            string[] cleaned = [.. value.Where(x => !string.IsNullOrWhiteSpace(x.Trim()))];
+
+            if (cleaned.Length == 0)
+            {
+                _supportedCultures = ["en", "vi"];
+                return;
+            }
+
+            _supportedCultures = cleaned;
+        }
+    }
     public int TranslationCacheInMinutes { get; set; } = 30;
 }
