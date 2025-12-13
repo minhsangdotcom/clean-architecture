0b76c7ab22266900c5b272292eab8288eeee233c
Minh Sáng <minhsang.1mil@gmail.com>
2025-12-13 19:51:04 +0700

    fix: cannot get id from reading json file region seeding

--- src/Domain/Aggregates/Regions/Commune.cs
+++ src/Domain/Aggregates/Regions/Commune.cs
@@ -1,8 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace Domain.Aggregates.Regions;
 
 public class Commune : Region
 {
+    public override Ulid Id { get; protected set; } = Ulid.NewUlid();
     public Ulid DistrictId { get; set; }
 
     public District? District { get; set; }
+
+    [JsonConstructor]
+    public Commune(Ulid id)
+    {
+        Id = id;
+    }
+
+    private Commune() { }
 }
