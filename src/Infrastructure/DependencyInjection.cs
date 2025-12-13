ac4fa4e65e7875fc9493b0cfe260a009a3234c7c
Minh Sáng <minhsang.1mil@gmail.com>
2025-12-13 12:27:08 +0700

    chore: remove unused using

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -15,7 +15,6 @@ using Infrastructure.Services.Token;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Npgsql;
