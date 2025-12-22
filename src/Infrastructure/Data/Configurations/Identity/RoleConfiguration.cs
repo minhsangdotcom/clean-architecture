1d919850a02674784affe10df3b72eeba20a6741
Minh Sáng <minhsang.1mil@gmail.com>
2025-12-22 15:15:49 +0700

    chore: update role name data type from text to citext

--- src/Infrastructure/Data/Configurations/Identity/RoleConfiguration.cs
+++ src/Infrastructure/Data/Configurations/Identity/RoleConfiguration.cs
@@ -9,5 +9,6 @@ public class RoleConfiguration : IEntityTypeConfiguration<Role>
     public void Configure(EntityTypeBuilder<Role> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasColumnType("citext");
     }
 }
---
+++ src/Infrastructure/Data/Migrations/20251222081232_UpdateRoleNameDataType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRoleNameDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "role",
+                type: "citext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "role",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "citext"
+            );
+        }
+    }
+}
